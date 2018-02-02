873b83518a93dd5111f3be571e467e7a10f2a148
wujiaming <wujiaming@iplas.com.cn>
2018-02-02 14:38:10 +0800

    提交

---
+++ src/main/java/com/demo/SpringBootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jimmy
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
